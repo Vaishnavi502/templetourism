8aab7c18c70f1865d5b31276060a3cc9f9756eb2
Vaishnavi Anand <68768878+Vaishnavi502@users.noreply.github.com>
2022-12-01 22:01:11 +0530

    Store booking details in database & send email
    Autogenerated email feature implemented

--- book_form.php
+++ book_form.php
@@ -1,8 +1,9 @@
 <?php
-   
+   // Obtain and store input from book.php into MySQL table 'book_db/book_form'
+   // To send automated email, must configure settings of php.ini (C:\xampp\php) and sendmail.ini (C:\xampp\sendmail) on server side
    ini_set('SMTP','ssl://smtp.gmail.com');
    ini_set('smtp_port',587);
-   ini_set("sendmail_from","vaishnavi.anand.official@gmail.com");
+   ini_set("sendmail_from","xyz@gmail.com");
    $connection = mysqli_connect('localhost','root','','book_db');
 
    if(isset($_POST['send']))
@@ -20,10 +21,12 @@
       mysqli_query($connection, $request);
 
       header('location:book.php');
+      
+      // Send email to client address with details entered in the form
       $mailto = $_POST['email'];
       $subject = "AUTOMATED MAIL FOR PACKAGE BOOKING CONFIRMATION";
       $body = "Hello {$_POST['name']}, the booking details for Package {$_POST['location']} is\nPhone: {$_POST['phone']}\nArrival date: {$_POST['arrivals']}\nDeparture date: {$_POST['leaving']}\n\nDo not reply to this email. This is auto-generated using SMTP and PHP.\nThank you for using the website created by Vaishnavi P, Vaishnavi Anand, Vandhana V and Umashankar";
-      $headers = "From: vaishnavi.anand.official@gmail.com \nReply-To: ".$mailto."\nX-Mailer: PHP/".phpversion();
+      $headers = "From: xyz@gmail.com \nReply-To: ".$mailto."\nX-Mailer: PHP/".phpversion();
       if(mail($mailto,$subject,$body,$headers))
          echo '<p>Confirmation email sent successfully</p>';
       else
